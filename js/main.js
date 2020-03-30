3a168e9c55b407fd4a2bc6fcbd89fb645020ad58
geewizz246 <geewizz246@users.noreply.github.com>
2020-03-30 11:07:46 -0400

    Finished GIMP Design for edit-profile page

--- js/main.js
+++ js/main.js
@@ -2,20 +2,23 @@ var controller = (function() {
     var EDIT_PROFILE_ITEMS = {
         firstName: document.getElementById('input-edit-first-name'),
         lastName: document.getElementById('input-edit-last-name'),
-        emails: {
+        email: {
             primary: document.getElementById('email-container-0'),
             alt1: document.getElementById('email-container-1'),
-            alt2: document.getElementById('email-container-2')
+            alt2: document.getElementById('email-container-2'),
+            countVisible: 1
         },
-        phones: {
+        phone: {
             primary: document.getElementById('phone-container-0'),
             alt1: document.getElementById('phone-container-1'),
-            alt2: document.getElementById('phone-container-2')
+            alt2: document.getElementById('phone-container-2'),
+            countVisible: 1
         },
-        addresses: {
+        address: {
             primary: document.getElementById('address-container-0'),
             alt1: document.getElementById('address-container-1'),
-            alt2: document.getElementById('address-container-2')
+            alt2: document.getElementById('address-container-2'),
+            countVisible: 1
         }
     };
 
@@ -23,50 +26,71 @@ var controller = (function() {
         document.getElementById('btn-add-email').addEventListener('click', addEmail);
         document.getElementById('btn-add-phone').addEventListener('click', addPhone);
         document.getElementById('btn-add-address').addEventListener('click', addAddress);
-        document.getElementById('exit').addEventListener('click', function(e) {
-            document.getElementById('v-pills-tab-container').classList.toggle('d-none');
-            e.preventDefault();
-        });
         document.querySelectorAll('.btn-remove-element').forEach((item) => {
             item.addEventListener('click', removeElement);
         });
     }
 
     var addEmail = function() {
-        for (email in EDIT_PROFILE_ITEMS.emails) {
-            if (EDIT_PROFILE_ITEMS.emails[email].classList.contains('d-none')) {
-                EDIT_PROFILE_ITEMS.emails[email].classList.toggle('d-none');
+        for (email in EDIT_PROFILE_ITEMS.email) {
+            if (EDIT_PROFILE_ITEMS.email[email].classList.contains('d-none')) {
+                EDIT_PROFILE_ITEMS.email[email].classList.toggle('d-none');
+                EDIT_PROFILE_ITEMS.email.countVisible++;
                 break;
             }
-    
-            document.getElementById('btn-add-email').classList.toggle('d-none');
+        }
+
+        console.log(EDIT_PROFILE_ITEMS.email.countVisible);
+
+        if (EDIT_PROFILE_ITEMS.email.countVisible === 3) { 
+            let btnAddEmail = document.getElementById('btn-add-email');
+            btnAddEmail.classList.toggle('d-none'); 
+            btnAddEmail.classList.toggle('d-flex');
         }
     };
 
     var addPhone = function() {
-        for (phone in EDIT_PROFILE_ITEMS.phones) {
-            if (EDIT_PROFILE_ITEMS.phones[phone].classList.contains('d-none')) {
-                EDIT_PROFILE_ITEMS.phones[phone].classList.toggle('d-none');
+        for (phone in EDIT_PROFILE_ITEMS.phone) {
+            if (EDIT_PROFILE_ITEMS.phone[phone].classList.contains('d-none')) {
+                EDIT_PROFILE_ITEMS.phone[phone].classList.toggle('d-none');
+                EDIT_PROFILE_ITEMS.phone.countVisible++;
                 break;
             }
-    
-            document.getElementById('btn-add-phone').classList.toggle('d-none');
+        }
+
+        if (EDIT_PROFILE_ITEMS.phone.countVisible === 3) { 
+            let btnAddPhone = document.getElementById('btn-add-phone');
+            btnAddPhone.classList.toggle('d-none'); 
+            btnAddPhone.classList.toggle('d-flex');
         }
     }
 
     var addAddress = function() {
-        for (address in EDIT_PROFILE_ITEMS.addresses) {
-            if (EDIT_PROFILE_ITEMS.addresses[address].classList.contains('d-none')) {
-                EDIT_PROFILE_ITEMS.addresses[address].classList.toggle('d-none');
+        for (address in EDIT_PROFILE_ITEMS.address) {
+            if (EDIT_PROFILE_ITEMS.address[address].classList.contains('d-none')) {
+                EDIT_PROFILE_ITEMS.address[address].classList.toggle('d-none');
+                EDIT_PROFILE_ITEMS.address.countVisible++;
                 break;
             }
-    
-            document.getElementById('btn-add-address').classList.toggle('d-none');
+        }
+
+        if (EDIT_PROFILE_ITEMS.address.countVisible === 3) { 
+            let btnAddAddress = document.getElementById('btn-add-address');
+            btnAddAddress.classList.toggle('d-none'); 
+            btnAddAddress.classList.toggle('d-flex');
         }
     }
 
     var removeElement = function(e) {
-        e.currentTarget.parentElement.classList.toggle('d-none')
+        let elementId = e.currentTarget.parentElement.id;
+        let type = elementId.substr(0, elementId.indexOf('-'));
+        let btn = document.getElementById(`btn-add-${type}`);
+
+        e.currentTarget.parentElement.classList.toggle('d-none');
+        EDIT_PROFILE_ITEMS[type].countVisible--;
+        
+        btn.classList.add('d-flex');
+        btn.classList.remove('d-none');
     }
 
     return {
